02af7acec7c6e7d6ec8d95b8b7bd7e2d379a4063
Nahuel <nahunavarro07@gmail.com>
2023-09-19 11:01:37 -0300

    Marcador mediante entry que lee coordenadas

--- MapApp/App.js
+++ MapApp/App.js
@@ -1,46 +1,93 @@
-import React from 'react';
-import MapView, {Marker} from 'react-native-maps';
-import { StyleSheet, View , Text, Dimensions, Button} from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import { StyleSheet, View, Text, Dimensions, Button, TextInput } from 'react-native';
 import { useEffect, useState } from 'react';
 import * as Location from 'expo-location';
-// import { Permissions } from 'expo-permissions';
-import { Permissions } from 'expo';
-
 
 export default function App() {
   const [mapRegion, setMapRegion] = useState({
     latitude: -31.31320035959943,
     longitude: -64.22334981122708,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
   });
+
+  const [markers, setMarkers] = useState([]); // State for markers
+  const [routeCoordinates, setRouteCoordinates] = useState([]); // State for route
+  const [newLatitude, setNewLatitude] = useState(''); // State for latitude input
+  const [newLongitude, setNewLongitude] = useState(''); // State for longitude input
 
   const userLocation = async () => {
     let { status } = await Location.requestForegroundPermissionsAsync();
-    
+
     if (status !== 'granted') {
       setErrorMsg('El acceso a la ubicación fue denegado');
       return;
     }
-  
+
     let location = await Location.getCurrentPositionAsync({ enableHighAccuracy: true });
     setMapRegion({
       latitude: location.coords.latitude,
       longitude: location.coords.longitude,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
     });
     console.log(location.coords.latitude, location.coords.longitude);
   };
 
-  useEffect(() =>{
+  const addMarker = () => {
+    if (newLatitude && newLongitude) {
+      const latitude = parseFloat(newLatitude);
+      const longitude = parseFloat(newLongitude);
+
+      // Create a new marker object
+      const newMarker = {
+        latitude,
+        longitude,
+        title: 'Custom Marker',
+      };
+
+      // Add the new marker to the markers state
+      setMarkers([...markers, newMarker]);
+    }
+  };
+
+  useEffect(() => {
     userLocation();
   }, []);
 
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} 
-      region={mapRegion}
-      >
-        <Marker coordinate={mapRegion} title='Multisoft'/>
+      <MapView style={styles.map} region={mapRegion}>
+        {markers.map((marker, index) => (
+          <Marker
+            key={index}
+            coordinate={{
+              latitude: marker.latitude,
+              longitude: marker.longitude,
+            }}
+            title={marker.title}
+          />
+        ))}
       </MapView>
-      <Button title='Get Location' onPress={userLocation} />
+      <TextInput
+        style={styles.input}
+        placeholder="Latitude"
+        onChangeText={(text) => setNewLatitude(text)}
+        value={newLatitude}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Longitude"
+        onChangeText={(text) => setNewLongitude(text)}
+        value={newLongitude}
+      />
+      <Button title="Add Marker" onPress={addMarker} />
+      <Button
+        title="Get Location"
+        onPress={() => {
+          userLocation();
+        }}
+      />
     </View>
   );
 }
@@ -50,7 +97,13 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   map: {
-    width: '100%',
-    height: '100%',
+    flex: 1,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    margin: 8,
+    padding: 8,
   },
 });

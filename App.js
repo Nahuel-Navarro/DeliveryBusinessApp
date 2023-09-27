04048b2df32ac1071f1b0cef1314cf4a11543858
NicolasVera23 <nicolasvera080@gmail.com>
2023-09-27 09:08:03 -0300

    Cambios 27/09 9:07

---
+++ App.js
@@ -0,0 +1,10 @@
+import {View, Text, StyleSheet, Image, TouchableOpacity, ScrollView} from 'react-native';
+
+  //Este lo que va a hacer es verificar si todos estan entregados
+  const todosEntregados = filteredClientes.every((cliente) => cliente.estado);
+  return(
+    <View>
+      
+    </View>
+  );
+
---
+++ src/pages/articulosComp/articulos.js
@@ -0,0 +1,13 @@
+import {View, Text, StyleSheet} from 'react-native';
+
+export function Articulos(){
+    return(
+        <View>
+            <Text>OLI   </Text>
+        </View>
+    )
+}
+
+const style = StyleSheet.create({
+    
+})
